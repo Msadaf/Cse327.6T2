9b980005ebbfb1e65e85eca8b477e77c2f7054ab
NAFISA ALAM <37190335+NafuKrypto@users.noreply.github.com>
2019-12-19 11:26:30 +0600

    After first failure commit a new ui design.:(

---
+++ app/src/main/java/com/example/digital_wallet/AddCardsAndCredentials.java
@@ -0,0 +1,16 @@
+package com.example.digital_wallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCardsAndCredentials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cards_and_credentials);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/digital_wallet/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.digital_wallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+
+/*
+@author nafisa alam
+@version 1.0
+@since since-19/dec/2019
+@deprecated represents home
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //add button on click function
+        /*
+@author nafisa alam
+
+@since since-19/dec/2019
+@deprecated represents button function
+
+
+ */
+        (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //class change
+           startActivity(new Intent(MainActivity.this,AddCardsAndCredentials.class));
+            }
+        });
+
+         /*
+@author nafisa alam
+
+@since since-19/dec/2019
+@deprecated represents spinner button catagory and choice function
+
+
+ */
+
+    }
+}
