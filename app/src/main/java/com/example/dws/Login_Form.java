c37665aea38fa2af26d294269ab8b91917af8c9c
mim <sadia.mim05@northsouth.edu>
2019-12-19 18:00:13 +0600

    Login SignUp  Commit

--- app/src/main/java/com/example/dws/Login_Form.java
+++ app/src/main/java/com/example/dws/Login_Form.java
@@ -6,17 +6,45 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login_Form extends AppCompatActivity {
-
+EditText password,contact_no;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__form);
         getSupportActionBar().setTitle("Login_Form");
+        password = (EditText) findViewById(R.id.pass1);
+        contact_no = (EditText) findViewById(R.id.Contact);
+        final String password_string = password.getText().toString().trim();
+        final String contact_no_string = contact_no.getText().toString().trim();
+
+        Button Login = (Button) findViewById(R.id.login);
+        Login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),password_string,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),contact_no_string,Toast.LENGTH_SHORT).show();
+                if (password_string=="") {
+                    password.setError("Enter password first");
+                }
+                if (contact_no_string=="") {
+                    contact_no.setError("Enter Your Phone Number");
+                }
+               else {
+                    startActivity(new Intent(getApplicationContext(), Home.class));//HOME e jabe class name
+                }
+            }
+
+        });
     }
 
-    public void btn_signUpform(View view) {
+    public void btn_Signup_Formm(View view) {
         startActivity(new Intent(getApplicationContext(),Signup_Form.class));
     }
 }
+
--- app/src/main/java/com/example/dws/Signup_Form.java
+++ app/src/main/java/com/example/dws/Signup_Form.java
@@ -2,13 +2,61 @@ package com.example.dws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/*@Author Sadia-mim
+Version @1.0
+Since 17.12.2019
+
+ */
 public class Signup_Form extends AppCompatActivity {
-
+EditText full_name,Contact,password,email,Birthdate;
+RadioGroup gender;
+RadioButton male,female;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__form);
+       full_name=(EditText) findViewById(R.id.Full_Name);
+       Contact=(EditText)findViewById(R.id.Contact);
+       password=(EditText)findViewById(R.id.Pass0);
+       email=(EditText)findViewById(R.id.email);
+       Birthdate=(EditText)findViewById(R.id.Birthdate);
+       gender=(RadioGroup)findViewById(R.id.genderradiogroup);
+       male=(RadioButton)findViewById(R.id.male);
+       female=(RadioButton)findViewById(R.id.female);
+       Button Submit=(Button)findViewById(R.id.submit);
+        Submit.setOnClickListener(new View.OnClickListener()
+        /*
+        @author Sadia-mim
+        @version 1.0
+        @Since 17.12.2019
+        @deprecated click submit button go to home class ,alternative go to login class
+        * */
+
+        {
+            @Override
+            public void onClick(View v) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+               // myRef.setValue("Hello, World!");
+                myRef.child("User").setValue("NAfisa Alam");
+                startActivity(new Intent(Signup_Form.this,Home.class));
+
+            }
+        });
+
+
     }
 }
