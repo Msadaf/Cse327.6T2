e3915e64ba1982985a51f2433bc935a2dc368990
mim <sadia.mim05@northsouth.edu>
2019-12-23 02:07:07 +0600

    Offer Done

--- app/src/main/java/com/example/dws/Login_Form.java
+++ app/src/main/java/com/example/dws/Login_Form.java
@@ -11,9 +11,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login_Form extends AppCompatActivity {
-EditText password,contact_no;
+    EditText password, contact_no;
     String contact_no_string;
-    int flag=1;
+    int flag = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,36 +23,40 @@ EditText password,contact_no;
         password = (EditText) findViewById(R.id.pass1);
         contact_no = (EditText) findViewById(R.id.Contact);
         final String password_string = password.getText().toString().trim();
-         contact_no_string = contact_no.getText().toString().trim();
+        contact_no_string = contact_no.getText().toString().trim();
 
         Button Login = (Button) findViewById(R.id.login);
         Login.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-               // Toast.makeText(getApplicationContext(),password_string,Toast.LENGTH_SHORT).show();
-               // Toast.makeText(getApplicationContext(),contact_no_string,Toast.LENGTH_SHORT).show();
-                if (password_string=="") {
+                // Toast.makeText(getApplicationContext(),password_string,Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(),contact_no_string,Toast.LENGTH_SHORT).show();
+                if (password_string == "") {
                     password.setError("Enter password first");
-                    flag=0;
+                    flag = 0;
                 }
-                if (contact_no_string=="") {
+                if (contact_no_string == "") {
                     contact_no.setError("Enter Your Phone Number");
-                    flag=0;
+                    flag = 0;
                 }
 
-               if(flag==1){
-                   //String phonenumber="+"+contact_no_string;
-                  // startActivity(new Intent(getApplicationContext(), Authentication.class));
-                   startActivity(new Intent(getApplicationContext(),Authentication.class));
-               }
+                if (flag == 1) {
+                    //String phonenumber="+"+contact_no_string;
+                    // startActivity(new Intent(getApplicationContext(), Authentication.class));
+                    startActivity(new Intent(getApplicationContext(), Authentication.class));
+
+                }
             }
 
         });
     }
 
     public void btn_Signup_Formm(View view) {
-        startActivity(new Intent(getApplicationContext(),Signup_Form.class));
+        startActivity(new Intent(getApplicationContext(), Signup_Form.class));
+    }
+    public void Offer (View view){
+        startActivity(new Intent(getApplicationContext(),Offer.class));
     }
 }
 
--- app/src/main/java/com/example/dws/Offer.java
+++ app/src/main/java/com/example/dws/Offer.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,38 +17,89 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Offer extends AppCompatActivity {
-    TextView offer1;
-    String value;
+    DatabaseReference database_Offer,database_Offer1;
+    TextView T0,T1,T2  ;
+    String value,value2,value3;
+    Button Next;
+
+    /*@author sadia-mim
+    @version 1.5
+    @param
+    @since 22.12.2019
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
-        offer1=(TextView)findViewById(R.id.offer1);
-        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
-        //DatabaseReference myRef = database.getReference("Order/DBBL");
+        T0 = (TextView) findViewById(R.id.T0);
+        T1=(TextView)findViewById(R.id.T1);
+        T2=(TextView)findViewById(R.id.T2);
+        Next = (Button) findViewById((R.id.Next));
+        database_Offer = FirebaseDatabase.getInstance().getReference().child("OFFER").child("HOP IN");
+        database_Offer1=FirebaseDatabase.getInstance().getReference().child("OFFER").child("DBBL");
 
-        // Read from the database
-        database.addValueEventListener(new ValueEventListener() {
 
-
+        Next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onClick(View v) {
+                database_Offer.addValueEventListener(new ValueEventListener() {
 
-                    // This method is called once with the initial value and again
-                    // whenever data at this location is updated.
-                    value = dataSnapshot.child("DBBL").getValue(String.class);
+                    /*@author sadia-mim
+                    @version 1.5
+                    @param object , return null value
+                    @since 22.12.2019
+                    @see https://www.youtube.com/watch?v=LpWhAz3e1sI&t=35s
+                     */
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        value = dataSnapshot.child("100% Discount").getValue().toString();
+                        value2=dataSnapshot.child("20% Discount").getValue().toString();
+                        try {
 
-                    offer1.setText(value);
-                    // Log.d(TAG, "Value is: " + value);
 
-            }
+                        }catch (Exception e){
+                            e.getMessage();
+                            Toast.makeText( getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-              //  Log.w(TAG, "Failed to read value.", error.toException());
+
+                        T0.setText(value);
+                        T1.setText(value2);
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+              });
+                database_Offer1.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        value3 = dataSnapshot.child("50% Discount").getValue().toString();
+                        T2.setText(value3);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
             }
         });
 
     }
-}
+
+
+        //DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        //DatabaseReference myRef = database.getReference("Order/DBBL");
+
+        // Read from the database
+        //database.addValueEventListener(new ValueEventListener() {
+
+
+    }
+
